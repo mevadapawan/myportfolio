386c527d55c0d7768d481c4a0b1d476f4010b8b4
mevadapawan <130583824+mevadapawan@users.noreply.github.com>
2023-04-12 21:04:50 +0530

    My Portfolio
    Welcome...

---
+++ script.js
@@ -0,0 +1,40 @@
+// HEADER TOGGLE  
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// TYPING EFFECT
+
+let typed=new Typed('.auto-input',{
+    strings:['Web Designer!','Front-End Developer!','UI Desiger!','Freelancer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
+
+// ACTIVE LINK STATE ON SCROLL
+
+// GET ALL LINKS 
+let navLinks = document.querySelectorAll('nav ul li a')
+// GET ALL SECTIONS
+let sections = document.querySelectorAll('section')
+console.log(sections)
+
+window.addEventListener('scroll', function() {
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') == link.getAttribute('href').substring(1))
+                    link.classList.add('active')
+            });
+        }
+    });
+});
